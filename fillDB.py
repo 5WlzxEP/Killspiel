d80a38414ceb92e51de6e0f978601fb5c3ffe1b8
5W_lzxEP <5w.lzxep@5wp.de>
2023-09-18 17:31:38 +0200

    update fillDB.py
    Signed-off-by: 5W_lzxEP <5w.lzxep@5wp.de>

--- fillDB.py
+++ fillDB.py
@@ -12,22 +12,22 @@ class Vote:
 
 class User:
     def __init__(self):
-        self._id: int = random.randint(0, 10_000_000)
-        self._name: str = faker.unique.name()
+        self._id: int = random.randint(0, 100_000_000)
+        self._name: str = (faker.name() + "_" + str(random.randint(0, 10000))).lower()
         self._guesses: int = 0
         self._points: int = 0
         self._latest: int = 0
 
         self._votes: [Vote] = []
 
-    def saveDB(self, cursor: sqlite3.Cursor):
+    def save_db(self, cursor: sqlite3.Cursor):
         cursor.execute(
             f"INSERT INTO Users VALUES ({self._id}, '{self._name}', {self._guesses}, {self._points}, {self._latest})")
 
         for vote in self._votes:
             cursor.execute(f"INSERT INTO Votes VALUES ({vote.game}, {self._id}, {vote.vote})")
 
-    def addVote(self, game: int, correct: int):
+    def add_vote(self, game: int, correct: int):
         vote = random.randint(0, 15)
         self._votes.append(Vote(game=game, vote=vote))
         self._guesses += 1
@@ -44,21 +44,26 @@ while True:
             cursor = db.cursor()
 
             users = []
-            for _ in range(5_000):
+            print("Generating Users")
+            for _ in range(2_000):
                 users.append(User())
 
-            for i in range(600):
+            for i in range(6_000):
+                if i % 100 == 0:
+                    print(f"Generating game {i}")
                 correct = random.randint(0, 15)
-                cursor.execute(f"INSERT INTO Game (correct) VALUES ({float(correct)});")
+                cursor.execute(f"INSERT INTO Game (correct, info) VALUES ({float(correct)}, 'generated test data');")
                 cursor.execute("SELECT last_insert_rowid()")
                 game = cursor.fetchone()[0]
 
                 for user in users:
-                    if random.randint(0, 10) < 8:
-                        user.addVote(game, correct)
+                    if random.randint(0, 10) < 5:
+                        user.add_vote(game, correct)
 
-            for user in users:
-                user.saveDB(cursor)
+            for i, user in enumerate(users):
+                if i % 10 == 0:
+                    print(f"saving user nr. {i}")
+                user.save_db(cursor)
 
             db.commit()
     except sqlite3.IntegrityError as e:
