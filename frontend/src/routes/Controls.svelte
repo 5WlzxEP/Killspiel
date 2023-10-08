df865088f8e93f0fcbfeafb465a2af524506ae4c
5W_lzxEP <5w.lzxep@5wp.de>
2023-10-08 21:41:47 +0200

    updated homepage
    Signed-off-by: 5W_lzxEP <5w.lzxep@5wp.de>

--- frontend/src/routes/Controls.svelte
+++ frontend/src/routes/Controls.svelte
@@ -54,28 +54,17 @@
 </script>
 
 <div class=" w-full h-full p-2">
+	<h1 class="text-lg text-center anchor">
+		<a href="/settings/data/manual/">Manuell & Status</a>
+	</h1>
 	<div class="text-center card p-2">
-		Aktueller Status: {printState($state)}
+		Aktueller Status: <h3 class="text-lg">{printState($state)}</h3>
 	</div>
 	<br />
 	{#if $state === 0}
-		<button
-			class="btn variant-ghost w-full mt-2"
-			type="button"
-			disabled={$state !== 0}
-			on:click={start}
-		>
-			Starten
-		</button>
+		<button class="btn variant-ghost w-full mt-2" type="button" on:click={start}> Starten </button>
 	{:else if $state === 1}
-		<button
-			class="btn variant-ghost w-full mt-2"
-			type="button"
-			disabled={$state !== 1}
-			on:click={stop}
-		>
-			Beenden
-		</button>
+		<button class="btn variant-ghost w-full mt-2" type="button" on:click={stop}> Beenden </button>
 	{:else if $state === 2}
 		<form class="p-2 gap-2 grid">
 			<input
