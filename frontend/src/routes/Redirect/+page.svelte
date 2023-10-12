df20d6f3d3f8201989686b5042283272a2e3b983
5W_lzxEP <5w.lzxep@5wp.de>
2023-10-12 21:03:32 +0200

    now uses replaceState => doesn't create browser history
    Signed-off-by: 5W_lzxEP <5w.lzxep@5wp.de>

--- frontend/src/routes/Redirect/+page.svelte
+++ frontend/src/routes/Redirect/+page.svelte
@@ -6,9 +6,9 @@
 		const loc = window.location.search
 		const decoded = decodeURI(loc).substring(1)
 		if (decoded === "" || decoded === "/Redirect" || decoded.startsWith("/r.html")) {
-			goto("/")
+			goto("/", {replaceState: true})
 			return
 		}
-		goto(decoded)
+		goto(decoded, {replaceState: true})
 	})
 </script>
